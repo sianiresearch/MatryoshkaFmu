3d8b58ceb43dd93eae6a29b50662cedce3573266
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2015-10-26 14:39:00 +0000

    Improves model description

--- src/org/javafmi/MatryoshkaModelDescription.java
+++ src/org/javafmi/MatryoshkaModelDescription.java
@@ -2,6 +2,10 @@ package org.javafmi;
 
 import org.javafmi.exporter.framework.modeldescription.*;
 
+import static org.javafmi.exporter.framework.modeldescription.ScalarVariableSpec.Causality.INPUT;
+import static org.javafmi.exporter.framework.modeldescription.ScalarVariableSpec.Causality.OUTPUT;
+import static org.javafmi.exporter.framework.modeldescription.ScalarVariableSpec.Variability.DISCRETE;
+
 public class MatryoshkaModelDescription {
     public static final String MODEL_NAME = "Matryoshka";
     public static final String SEDAN_SPEED = "sedan.speed";
@@ -12,15 +16,41 @@ public class MatryoshkaModelDescription {
     public static ModelDescription build() {
         return ModelDescription
                 .from(new ModelDescriptionSpec(new ModelDescriptionAttributes(MODEL_NAME))
-                        .withVariablesSpec(configureVariables(new ModelVariablesSpec())));
+                        .withVariablesSpec(variables()));
     }
 
-    private static ModelVariablesSpec configureVariables(ModelVariablesSpec variables) {
-        variables.add(new RealVariableSpec(new ScalarVariableSpec(SEDAN_SPEED)))
-                .add(new RealVariableSpec(new ScalarVariableSpec(COUPE_SPEED)))
-                .add(new RealVariableSpec(new ScalarVariableSpec(SEDAN_DISTANCE)))
-                .add(new RealVariableSpec(new ScalarVariableSpec(COUPE_DISTANCE)));
-        return variables;
+    private static ModelVariablesSpec variables() {
+        return new ModelVariablesSpec()
+                .add(sedanSpeed())
+                .add(coupeSpeed())
+                .add(sedanDistance())
+                .add(coupeDistance());
+    }
+
+    private static RealVariableSpec sedanSpeed() {
+        return new RealVariableSpec(new ScalarVariableSpec(SEDAN_SPEED)
+                .causality(INPUT)
+                .variability(DISCRETE))
+                .start(Matryoshka.INITIAL_SPEED);
+    }
+
+    private static RealVariableSpec coupeSpeed() {
+        return new RealVariableSpec(new ScalarVariableSpec(COUPE_SPEED)
+                .causality(INPUT)
+                .variability(DISCRETE))
+                .start(Matryoshka.INITIAL_SPEED);
+    }
+
+    private static RealVariableSpec sedanDistance() {
+        return new RealVariableSpec(new ScalarVariableSpec(SEDAN_DISTANCE)
+                .causality(OUTPUT)
+                .variability(DISCRETE));
+    }
+
+    private static RealVariableSpec coupeDistance() {
+        return new RealVariableSpec(new ScalarVariableSpec(COUPE_DISTANCE)
+                .causality(OUTPUT)
+                .variability(DISCRETE));
     }
 
 }
